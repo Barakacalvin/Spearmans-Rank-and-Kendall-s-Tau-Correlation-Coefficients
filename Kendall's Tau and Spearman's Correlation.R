9c36aebbb4504893f29222c5814f5019e5324f86
Calvin Baraka <30375880+Barakacalvin@users.noreply.github.com>
2024-12-29 15:20:50 +0300

    Update Kendall's Tau and Spearman's Correlation.R

--- Kendall's Tau and Spearman's Correlation.R
+++ Kendall's Tau and Spearman's Correlation.R
@@ -1,22 +1,25 @@
 # Load required libraries
 library(tidyverse)
 
-# Load the dataset with error handling
+# Loading the dataset with error handling of the data import
 file_path <- "File_Path - N/B Should be separated by Foward slashes"
+# Checking if file exists
 if (file.exists(file_path)) {
   data <- read.csv(file_path)
+# Checking if the variables being tested exist (check for the existing column names)
   if (!("prody" %in% colnames(data)) | !("commit" %in% colnames(data))) {
     stop("Columns 'prody' and/or 'commit' do not exist in the dataset.")
   }
+# Error message fro when file is not available
 } else {
   stop("File not found.")
 }
 
-# Calculate Spearman's rank correlation
+# Calculation of the Spearman's rank correlation
 correlation_spearman <- cor(data$prody, data$commit, method = "spearman")
-print(paste("Spearman's rho:", correlation_spearman))
+print(paste("Spearman's rho:", correlation_spearman)) #Prints out the results of Spearmans computation
 
-# Calculate Kendall's tau correlation
+# Calculation of the Kendall's tau correlation
 correlation_kendall <- cor(data$prody, data$commit, method = "kendall")
-print(paste("Kendall's tau:", correlation_kendall))
+print(paste("Kendall's tau:", correlation_kendall)) #Prints out the results of Kendall's computation
 
